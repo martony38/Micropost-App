c9e63445027021cb73fefc8b1616224f18fe8e29
Laurent Sehabiague <martony@gmail.com>
2016-05-09 15:36:53 -0400

    Finish layout and routes

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe "static_pages/home.html.erb", type: :view do
     	render template: "static_pages/home.html.erb", layout: "layouts/application.html.erb"
       	assert_select "title", "Ruby on Rails Tutorial Sample App"
     end
+    it "displays the links to the home page" do
+		render template: "static_pages/home.html.erb", layout: "layouts/application.html.erb"
+		assert_select "a[href=?]", root_path, count: 2
+	end
+    it "displays the link to the help page" do
+		render template: "static_pages/home.html.erb", layout: "layouts/application.html.erb"
+		assert_select "a[href=?]", help_path
+	end
+    it "displays the link to the about page" do
+		render template: "static_pages/home.html.erb", layout: "layouts/application.html.erb"
+		assert_select "a[href=?]", about_path
+	end
+    it "displays the link to the contact page" do
+		render template: "static_pages/home.html.erb", layout: "layouts/application.html.erb"
+		assert_select "a[href=?]", contact_path
+	end
 end
